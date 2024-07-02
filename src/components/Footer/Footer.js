23b1eea8f951d213a2c636d4c6d384279defe746
bhavin-08 <bhavinrathod1115@gmail.com>
2024-07-02 21:00:55 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/bhavin-08/my_portfolio'
       className='link footer__link'
     >
       Created By Bhavin Rathod
